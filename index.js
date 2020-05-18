04f829b5ec16f9f0054efa0217e6a061d57e8ccf
ducmk254 <ducmk254@gmail.com>
2020-05-19 03:27:55 +0700

    install nodemon to auto restart server when index.js was changed

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 let app = express();
+const port = 3001;
 
 app.set('view engine','pug'); // khai bao view engine
 app.set('views','./views');   // khai bao path cho views
 
 
 app.get('/',(req,res)=>{{
-    res.render('home.pug');
-}})
+    res.render('home');
+}});
+
+app.get('/learn',(req,res)=>{
+    res.render('learn');
+});
 
 
 
-app.listen(3001,()=>console.log('Server started...'));
+
+app.listen(port,()=>console.log('Server started with port '+ port +'...'));
