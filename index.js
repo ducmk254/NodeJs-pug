e616fec2dc60563dc8515a285938bfdd466a2df3
ducmk254 <ducmk254@gmail.com>
2020-05-19 04:03:48 +0700

    fix reload page auto by changed source code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 let app = express();
 const port = 3001;
+const reload = require('reload'); //eslint-disable-line
+
+
+
+
 
 app.set('view engine','pug'); // khai bao view engine
 app.set('views','./views');   // khai bao path cho views
@@ -15,6 +20,13 @@ app.get('/learn',(req,res)=>{
 });
 
 
+//Reload code here
+// const server = require('http').createServer(app);
+// reload(app).then(()=>{
+//     server.listen(app.get('port'),()=>{
+//         console.log('Server started with port '+ app.get('port') +'...');
+//     });
+// });
 
-
-app.listen(port,()=>console.log('Server started with port '+ port +'...'));
+reload(app);
+app.listen(port,()=>console.log('Server is started with :' + port + ' port'));
