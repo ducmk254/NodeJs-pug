c3d3ca3c1509bb0f49bbac5e32d985e417b75837
ducmk254 <ducmk254@gmail.com>
2020-05-19 06:20:35 +0700

    include in pug template

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 let app = express();
 const port = 3001;
 const reload = require('reload'); //eslint-disable-line
+const arrCBNV = [{name:'Minh Đức', age: 3},{name:'Minh Tâm', age: -1},{name:'Khánh Chi', age: 8},{name:'Khánh Ngọc', age: 9}];
+const arrUsers = [{username:'admin'},{username:'minhduc'},{username:'conga'}];
+
+// Cách 1:  truyền biến arrCBNV từ index sang file pug : b1: gán thường arrCBNVs cho biến locals arrCBNV 
+// chỉ thích hợp với dữ liệu dùng chung. vd dữ liệu về danh sách sản phẩm , dữ liệu về username, password không làm như thế đc vì mỗi ng có 1 dữ liệu khac nhau
+
+// app.locals.arrCBNV = arrCBNVs; 
+
+
 
 
 
@@ -15,8 +24,11 @@ app.get('/',(req,res)=>{{
     res.render('home');
 }});
 
+
+// Cách 2: truyền thông qua method get/post , truyền đối số thứ 2 trong res.render()
 app.get('/learnPug',(req,res)=>{
-    res.render('learn');
+    res.render('learn',{arrCBNV:arrCBNV,arrUsers}); // bên /learn sẽ có được biến arrCBNV là mảng arrCBNV
+    // tên của biến arrCBNV và mảng arrCBNV giống nhau nên có thể viết gọn lại là: res.render('learn',{arrCBNV});
 });
 
 app.get('/admin',(req,res)=>{
